348cf510ddb98bb824b247cd0367cb9b3ac0a84f
Adryan Alencar <50060295+AdryanAlencar@users.noreply.github.com>
2022-02-21 14:41:21 -0300

    Ignore self signed certificates

--- src/services/GitLabApi.ts
+++ src/services/GitLabApi.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
+import https from 'https';
 
 function GitLabApi(){
     this.baseUrl = `${process.env.SECURE_URL ? 'https' : 'http'}://${process.env.BASE_URL}/api/v4`;
-
     this.headers = {
         'Private-Token': process.env.PRIVATE_TOKEN,
     }
-
+    
+    const agent = new https.Agent({  
+        rejectUnauthorized: false
+    });
+    
     this.ObjectToQueryString = (obj) => {
         return '?' + Object.keys(obj).map(k => {
             return encodeURIComponent(k) + '=' + encodeURIComponent(obj[k])
@@ -15,7 +19,7 @@ function GitLabApi(){
 
     this.getRelease = async (projectId, releaseName) => {
         const url = `${this.baseUrl}/projects/${projectId}/releases`;
-        const response = await axios.get(url, { headers: this.headers });
+        const response = await axios.get(url, { headers: this.headers, httpsAgent: agent });
         const release = response.data.find(release => release.tag_name === releaseName);
 
         return release;
@@ -25,7 +29,7 @@ function GitLabApi(){
         const url = `${this.baseUrl}/projects/${projectId}/releases/${releaseName}/assets/links`;
         var data = this.ObjectToQueryString(link)
 
-        const response = await axios.post(url, data, { headers: this.headers });
+        const response = await axios.post(url, data, { headers: this.headers, httpsAgent: agent });
 
         return response.data;
     }
@@ -35,19 +39,19 @@ function GitLabApi(){
         const url = `${this.baseUrl}/projects/${projectId}/releases/${releaseName}/assets/links/${link.id}`;
         var data = this.ObjectToQueryString(link)
 
-        const response = await axios.put(url, data, { headers: this.headers });
+        const response = await axios.put(url, data, { headers: this.headers, httpsAgent: agent });
 
         return response.data;
     }
 
     this.createProjectHook = async (projectId) => {
         const url = `${this.baseUrl}/projects/${projectId}/hooks`;
-        var data = this.objectToQueryString({
+        var data = this.ObjectToQueryString({
             url: `http://localhost:${process.env.PORT}/webhook`,
             release_events: true,
         });
 
-        const response = await axios.post(url, data, { headers: this.headers });
+        const response = await axios.post(url, data, { headers: this.headers, httpsAgent: agent });
 
         return response.data;
     }      
